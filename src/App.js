97595ec8d27b49b8e4d565a7a368291f52077e40
Anton <antonwar04@gmail.com>
2024-12-26 02:17:15 +0300

    chore: Edit App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект Лямкина Антона Александровича";
+  const greeting = "Это тестовый проект Лямкина А.А.";
 
   
   return (
